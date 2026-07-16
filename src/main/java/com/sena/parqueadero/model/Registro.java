8f70067ecc1225ab1891c7a1df329d31e8d11e41
Eliana Piñeros <eli_pineros@gmail.com>
2026-07-15 21:51:51 -0500

    v0.1_clasesPaqueteModelCompletas_eliVic_15072026

--- src/main/java/com/sena/parqueadero/model/Registro.java
+++ src/main/java/com/sena/parqueadero/model/Registro.java
@@ -1,5 +1,40 @@
 package com.sena.parqueadero.model;
 
-public class Registro {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "registros")
+
+public class Registro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idRegistro;
+	@Column(nullable = false)
+	private LocalDateTime fechaDateTime;
+	private LocalDateTime fechaSalida;
+	private Double valorPagado;
+	private Boolean activo = true;
+
+	@ManyToOne
+	@JoinColumn(name = "idVehiculo", nullable = false)
+	private Vehiculo vehiculo;
 }
